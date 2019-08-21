6a751a8a79c5fd20aeff661d49060f5c67c3a8be
slieman <slieman2001@hotmail.com>
2019-08-21 12:47:33 +0300

    add AlertRadio

--- src/app/components/alert/alert.component.ts
+++ src/app/components/alert/alert.component.ts
@@ -13,7 +13,7 @@ export class AlertComponent implements OnInit {
   ngOnInit() {}
 
   showAlert() {
-    this.presentAlertPrompt();
+    this.presentAlertRadio();
   }
   async presentAlert() {
     const alert = await this.alertController.create({
@@ -113,4 +113,67 @@ export class AlertComponent implements OnInit {
 
     await alert.present();
   }
+
+  async presentAlertRadio() {
+    const alert = await this.alertController.create({
+      header: 'Radio',
+      inputs: [
+        {
+          name: 'radio1',
+          type: 'radio',
+          label: 'Radio 1',
+          value: 'value1',
+          checked: true
+        },
+        {
+          name: 'radio2',
+          type: 'radio',
+          label: 'Radio 2',
+          value: 'value2'
+        },
+        {
+          name: 'radio3',
+          type: 'radio',
+          label: 'Radio 3',
+          value: 'value3'
+        },
+        {
+          name: 'radio4',
+          type: 'radio',
+          label: 'Radio 4',
+          value: 'value4'
+        },
+        {
+          name: 'radio5',
+          type: 'radio',
+          label: 'Radio 5',
+          value: 'value5'
+        },
+        {
+          name: 'radio6',
+          type: 'radio',
+          label: 'Radio 6 Radio 6 Radio 6 Radio 6 Radio 6 Radio 6 Radio 6 Radio 6 Radio 6 Radio 6 ',
+          value: 'value6'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: () => {
+            console.log('Confirm Cancel');
+          }
+        }, {
+          text: 'Ok',
+          handler: () => {
+            console.log('Confirm Ok');
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
+
 }
