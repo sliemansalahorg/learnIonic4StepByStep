c2ad7005ed809f5672f28769add1ab8379cb6ee6
slieman <slieman2001@hotmail.com>
2019-08-21 12:46:42 +0300

    add AlertPrompt

--- src/app/components/alert/alert.component.ts
+++ src/app/components/alert/alert.component.ts
@@ -13,7 +13,7 @@ export class AlertComponent implements OnInit {
   ngOnInit() {}
 
   showAlert() {
-    this.presentAlertConfirm();
+    this.presentAlertPrompt();
   }
   async presentAlert() {
     const alert = await this.alertController.create({
@@ -42,6 +42,70 @@ export class AlertComponent implements OnInit {
           text: 'Okay',
           handler: () => {
             console.log('Confirm Okay');
+          }
+        }
+      ]
+    });
+    await alert.present();
+  }
+
+  async presentAlertPrompt() {
+    const alert = await this.alertController.create({
+      header: 'Prompt!',
+      inputs: [
+        {
+          name: 'name1',
+          type: 'text',
+          placeholder: 'Placeholder 1'
+        },
+        {
+          name: 'name2',
+          type: 'text',
+          id: 'name2-id',
+          value: 'hello',
+          placeholder: 'Placeholder 2'
+        },
+        {
+          name: 'name3',
+          value: 'http://ionicframework.com',
+          type: 'url',
+          placeholder: 'Favorite site ever'
+        },
+        // input date with min & max
+        {
+          name: 'name4',
+          type: 'date',
+          min: '2017-03-01',
+          max: '2018-01-12'
+        },
+        // input date without min nor max
+        {
+          name: 'name5',
+          type: 'date'
+        },
+        {
+          name: 'name6',
+          type: 'number',
+          min: -5,
+          max: 10
+        },
+        {
+          name: 'name7',
+          type: 'number'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: () => {
+            console.log('Confirm Cancel');
+          }
+        }, {
+          text: 'Ok',
+          handler: () => {
+            console.log('Confirm Ok');
           }
         }
       ]
