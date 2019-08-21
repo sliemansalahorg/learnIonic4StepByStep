106239d61ed06014f248d536604c8d3f91eaa246
slieman <slieman2001@hotmail.com>
2019-08-21 15:51:22 +0300

    add ion-slide, ion-avatar, ion-img, ion-select with templates on about page...

--- src/app/about/about.page.ts
+++ src/app/about/about.page.ts
@@ -6,7 +6,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.page.scss'],
 })
 export class AboutPage implements OnInit {
+  slideOpts = {
+    initialSlide: 0,
+    speed: 400
+  };
+  customAlertOptions: any = {
+    header: 'Pizza Toppings',
+    subHeader: 'Select your toppings',
+    message: '$1.00 per topping',
+    translucent: true
+  };
 
+  customPopoverOptions: any = {
+    header: 'Hair Color',
+    subHeader: 'Select your hair color',
+    message: 'Only select your dominant hair color'
+  };
+
+  customActionSheetOptions: any = {
+    header: 'Colors',
+    subHeader: 'Select your favorite color'
+  };
+  users: any[] = [
+    {
+      id: 1,
+      first: 'Alice',
+      last: 'Smith',
+    },
+    {
+      id: 2,
+      first: 'Bob',
+      last: 'Davis',
+    },
+    {
+      id: 3,
+      first: 'Charlie',
+      last: 'Rosenburg',
+    }
+  ];
+  compareWithFn = (o1, o2) => {
+    return o1 && o2 ? o1.id === o2.id : o1 === o2;
+  }
+  // tslint:disable-next-line:member-ordering
+  compareWith = this.compareWithFn;
   constructor() { }
 
   ngOnInit() {
