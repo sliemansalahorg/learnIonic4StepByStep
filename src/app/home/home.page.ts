ef281754ea9dadc848dddb4da6469bf52a03a397
slieman <slieman2001@hotmail.com>
2019-08-21 16:12:40 +0300

    Default Progressbar

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -11,29 +11,5 @@ export class HomePage implements OnInit {
   constructor(public loadingController: LoadingController) { }
 
   ngOnInit() {
-    this.presentLoadingWithOptions();
-  }
-
-  async presentLoading() {
-    const loading = await this.loadingController.create({
-      message: 'Hellooo',
-      duration: 2000
-    });
-    await loading.present();
-
-    const { role, data } = await loading.onDidDismiss();
-
-    console.log('Loading dismissed!');
-  }
-
-  async presentLoadingWithOptions() {
-    const loading = await this.loadingController.create({
-      spinner: null,
-      duration: 5000,
-      message: 'Please wait...',
-      translucent: true,
-      cssClass: 'custom-class custom-loading'
-    });
-    return await loading.present();
   }
 }
