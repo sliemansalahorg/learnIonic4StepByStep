85bbf587a875715111d7b980d6345c7b1a0e0c5c
slieman <slieman2001@hotmail.com>
2019-08-21 12:18:39 +0300

    add action sheet component to module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,10 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { ActionSheetComponent } from './components/action-sheet/action-sheet.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ActionSheetComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [
