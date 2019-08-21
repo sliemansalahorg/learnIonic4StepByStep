71cf21f7bdb9b0a6b69157bbf92f6ddc149c9e4a
slieman <slieman2001@hotmail.com>
2019-08-21 16:38:17 +0300

    ion-nav used...

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,28 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-import { PopoverController } from '@ionic/angular';
-import { ActionSheetComponent } from '../components/action-sheet/action-sheet.component';
-
+import { NavController } from '@ionic/angular';
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
 })
 export class HomePage implements OnInit {
-  constructor(public popoverController: PopoverController) { }
+  constructor(public nav: NavController) { }
 
   ngOnInit() {
   }
 
-  popOver() {
-    this.presentPopover(event);
-  }
-
-  async presentPopover(ev: any) {
-    const popover = await this.popoverController.create({
-      component: ActionSheetComponent,
-      event: ev,
-      translucent: true
-    });
-    return await popover.present();
+  loadUsersPage() {
+    this.nav.navigateRoot('tabs/users');
   }
 }
